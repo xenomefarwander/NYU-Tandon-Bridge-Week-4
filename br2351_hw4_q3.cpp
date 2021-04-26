b857ee6296532ac5df9931f5080ce8e419bd6b3b
Brandon W. Roddey <broddey@gmail.com>
2021-04-25 22:55:22 -0400

    simplified comments

--- br2351_hw4_q3.cpp
+++ br2351_hw4_q3.cpp
@@ -8,50 +8,41 @@ using namespace std;
 int main() {
     int inputInt;
     int logCalc;
-    int log2Int, exponent, cursorPos;
-    int twoExpLog2;
-    int remainderInt;
+    int log2Curr, exponent, log2Mem;
+    int powOf2;
+    int remainderCurr;
 
     cout << "Enter decimal number:" << endl;
     cin >> inputInt;
     cout << "The binary representation of " << inputInt << " is ";
 
-    remainderInt = inputInt;
-    cursorPos = 0;
-    while (remainderInt > 0){
-        // Calculate integer value of the remainder represented as 2^x, where x = log2Int(remainderInt). Value of log2Int is
-        // calculated by incrementing after every division operation until remainder no longer divisible by 2. The
-        // resulting 'log2Int' is an integer value.
-        for (logCalc = remainderInt, log2Int = 0; logCalc > 1; log2Int += 1) {
+    remainderCurr = inputInt;
+    log2Mem = 0;
+    while (remainderCurr > 0){
+        // Calculate log2Curr
+        for (logCalc = remainderCurr, log2Curr = 0; logCalc > 1; log2Curr += 1) {
             logCalc /= 2;
         }
-
-        // This segment only activates starting from the 2nd iteration: cursorPos represents the previous value of
-        // log2Int(remainderInt). The cursorPos is moved to right until reaching the current 2^x bit, printing a zero
-        // each time but leaving a space at the end to print the 1 in the correct bit.
-        // (i.e. number of zeroes to print equals log2Int(remainder1) - (log2Int(remainder2) + 1))
-        while (cursorPos > log2Int + 1){
+        // Print residual zeroes
+        while (log2Mem > log2Curr + 1){
             cout << "0";
-            cursorPos -= 1;
+            log2Mem -= 1;
         }
-
-        // Print 1 for current 2^x bit
+        // Print 1 for current 2^x bit and set log2Mem to current value of log2Curr
         cout << 1;
-        // After printing the 1, cursorPos is equal to value of log2Int (number of bits remaining to the right of the 1)
-        cursorPos = log2Int;
+        log2Mem = log2Curr;
 
-        // Calculate value of 2^log2Int (e.g. 2^6 results in 64, the value to be subtracted from remainderInt)
-        for (exponent = log2Int, twoExpLog2 = 1; exponent > 0; exponent -=1)
-            twoExpLog2 *= 2;
-
-        // Reassign remainderInt for purposes of the next loop iteration (e.g. "76 - 64" = 12)
-        remainderInt = remainderInt - twoExpLog2;
+        // Calculate value of 2^x (e.g. 2^6 results in 64, the value to be subtracted from remainderCurr)
+        for (exponent = log2Curr, powOf2 = 1; exponent > 0; exponent -=1)
+            powOf2 *= 2;
+        // Reassign remainderCurr for purposes of the next loop iteration (e.g. "76 - 64" = 12)
+        remainderCurr %= powOf2;
     }
 
-    // Print remaining zeroes to screen when remainderInt = 0 and log2Int > 0
-    while (cursorPos > 0) {
+    // Print remaining zeroes to screen when remainderCurr = 0 and log2Curr > 0
+    while (log2Mem > 0) {
         cout << 0;
-        cursorPos -= 1;
+        log2Mem -= 1;
     }
 
     // Finalize output and print 0 to screen in the case user entered 0 as input
